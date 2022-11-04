7b7663c1f50cfecca1bf80f56ce3b521f389b109
Juan Daniel Padilla <juand.padilla@xaldigital.com>
2022-11-04 16:24:54 +0000

    example questions data

--- frontend-maths/src/assets/data.js
+++ frontend-maths/src/assets/data.js
@@ -1,69 +1,11 @@
 export const data = [
     {
-        question: 'What is 2 + 2?',
+        question: 'Example question?',
         answers: [
-            { text: '4', correct: true },
-            { text: '5', correct: false },
-            { text: '6', correct: false },
-            { text: '7', correct: false },
-        ],
-    },
-    {
-        question: 'What is 3 + 3?',
-        answers: [
-            { text: '4', correct: false },
-            { text: '5', correct: false },
-            { text: '6', correct: true },   
-            { text: '7', correct: false },
-        ],
-    },
-    {
-        question: '¿Cual es el mejor numero?',
-        answers: [
-            { text: '1337', correct: false },
-            { text: '420', correct: false },
-            { text: '666', correct: false },
-            { text: '69', correct: true },
-        ],
-    },
-    {
-        question: '¿Cuanta es la distancia de la luna al sol??',
-        answers: [
-            { text: 'Si', correct: true },
-            { text: 'No', correct: false },
-        ],
-    },
-    {
-        question: '¿Qué frase celebre dijo Bad bunny ?',
-        answers: [
-            { text: 'No hay hombre tan cobarde a quien el amor no haga valiente y transforme en héroe.', correct: false },
-            { text: 'Hay alguien tan inteligente que aprende de la experiencia de los demás.', correct: false },
-            { text: 'No puedo enseñar nada a nadie. Solo puedo hacerles pensar.', correct: false },
-            { text: 'Y diles Que yo me sé tu pose favoritas Que te hablo malo y que eso te excita', correct: true },
-        ],
-    },
-    {
-        question: '¿CUanto es 1+1?',
-        answers: [
-            { text: '2', correct: true },
-            { text: '11', correct: false },
-            { text: '43', correct: false },
-            { text: '1+1', correct: true },
-        ],
-    },
-    {
-        question: '¿Que Frase no dice Darell?',
-        answers: [
-            { text: 'La verdadera vuelta', correct: false },
-            { text: 'tu me estas entendiendo lo que te quiero decir', correct: false },
-            { text: 'eso es así', correct: false },
-            { text: 'por ley', correct: false },
-            { text: 'pa que sepas', correct: false },
-            { text: 'ratatatata', correct: false },
-            { text: 'everybody go ot the discotek', correct: false },
-            { text: 'Hi sweety', correct: false },
-            { text: 'BRRRRRRR', correct: true },
-            { text: 'Woh Oh', correct: false },
+            { text: 'Example answer 1', correct: true },
+            { text: 'Example answer 2', correct: false },
+            { text: 'Example answer 3', correct: false },
+            { text: 'Example answer 4', correct: false },
         ],
     },
 ]
