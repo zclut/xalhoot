a74acef8caf7ac69300a850a92c9fd0f54db0b52
Juan Daniel Padilla <juand.padilla@xaldigital.com>
2022-12-14 13:02:25 +0000

    Adding data

--- client/src/assets/data.js
+++ client/src/assets/data.js
@@ -1,11 +1,108 @@
 export const data = [
     {
-        question: 'Example question?',
+        question: '¿Sabes responder preguntas?',
         answers: [
-            { text: 'Example answer 1', correct: true },
-            { text: 'Example answer 2', correct: false },
-            { text: 'Example answer 3', correct: false },
-            { text: 'Example answer 4', correct: false },
+            { text: 'Si', correct: false },
+            { text: 'No', correct: false },
+        ],
+    },
+    {
+        question: '¿Qué es Terraform?',
+        answers: [
+            { text: 'Es un CPD Virtual', correct: false },
+            { text: 'Es un software IDE', correct: false },
+            { text: 'Es un lenguaje de infraestructura como código', correct: true },
+            { text: 'Un servicio de amazon', correct: false },
+        ],
+    },
+    {
+        question: 'Route53 es un servicio de...',
+        answers: [
+            { text: 'DNS', correct: true },
+            { text: 'DHCP', correct: false },
+            { text: 'Dinner', correct: false },
+            { text: 'DWH', correct: false },
+        ],
+    },
+    {
+        question: '¿Qué es CI/CD?',
+        answers: [
+            { text: 'Continuous Information Continuous Domination', correct: false },
+            { text: 'Continuous Integration Continuous DevOps', correct: false },
+            { text: 'Continuous Integration Continuous Destiny', correct: false },
+            { text: 'Continuous Integration Continuous Delivery', correct: true },
+        ],
+    },
+    {
+        question: '¿Qué nos ayuda a manejar la latencia del contenido estático?',
+        answers: [
+            { text: 'Amazon Gitlab', correct: false },
+            { text: 'CloudFront', correct: true },
+            { text: 'React', correct: false },
+            { text: 'EC2', correct: false },
+        ],
+    },
+    {
+        question: '¿En que servicio de Amazon podriamos almacenar una imagen?',
+        answers: [
+            { text: 'Amazon Jenkins', correct: false },
+            { text: 'DynamoDB', correct: false },
+            { text: 'Amazon S3', correct: true },
+            { text: 'Terraform', correct: false },
+        ],
+    },
+    {
+        question: 'Elige la más correcta; siguiendo la filosofia DevOps...',
+        answers: [
+            { text: 'Podemos automatizar todo nuestro trabajo', correct: false },
+            { text: 'Podemos automatizar tests/pruebas unitarias', correct: false },
+            { text: 'Podemos automatizar el despliegue', correct: false },
+            { text: 'Podemos automatizar la gran mayoria de tareas', correct: true },
+        ],
+    },
+    {
+        question: '¿Donde tenemos mejor rendimiento alojando el servidor?',
+        answers: [
+            { text: 'EC2', correct: true },
+            { text: 'Amazon Upgrade Perfomance', correct: false },
+            { text: 'EC2 + Render', correct: false },
+            { text: 'Render', correct: false },
+        ],
+    },
+    {
+        question: 'Con que libreria esta desarrollado el frontend de la aplicación',
+        answers: [
+            { text: 'Angular', correct: false },
+            { text: 'Apollo Server', correct: false },
+            { text: 'GraphQL', correct: false },
+            { text: 'React', correct: true },
+        ],
+    },
+    {
+        question: '¿Cuál es una fase del CI/CD?',
+        answers: [
+            { text: 'Code', correct: true },
+            { text: 'Searching', correct: false },
+            { text: 'Typing', correct: false },
+            { text: 'Instalar instancias EC2', correct: false },
+        ],
+    },
+    {
+        question: 'Si tenemos un CPD en físico se llama...',
+        answers: [
+            { text: 'Cloud', correct: false },
+            { text: 'On premises', correct: false },
+            { text: 'On premises o Hibrido', correct: true },
+            { text: 'Hibrido', correct: false },
+        ],
+    },
+    {
+        question: '¿Como se llaman las mascotas de la empresa?',
+        answers: [
+            { text: 'Los erizos hermanos', correct: false },
+            { text: 'Xalvino y Spelunky', correct: false },
+            { text: 'Xalvy e Spiky', correct: true },
+            { text: 'Xavi y Espinas', correct: false },
         ],
     },
 ]
